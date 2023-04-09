75bb30747c888d55298f543f347d9b403cc69f2b
hcgo97 <hcgo_o@naver.com>
2023-04-10 02:33:35 +0900

    ✏️edit gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `🤖 HyoJ's WiKi`,
+    siteTitleAlt: `HyoJ's WiKi`,
+    siteHeadline: `HyoJ's WiKi`,
+    siteUrl: `https://hyoj.me`,
+    siteDescription: `HyoJ's Dev Blog`,
     siteImage: `/banner.jpg`,
-    siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    siteLanguage: `ko`,
+    author: `@hyoj`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -29,18 +29,14 @@ const config: GatsbyConfig = {
             slug: `/blog`,
           },
           {
-            title: `About`,
-            slug: `/about`,
+            title: `Tags`,
+            slug: `/tags`,
           },
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `About`,
+            url: `/about`,
           },
         ],
       },
